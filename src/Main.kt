d99776faf0b26b29f9457ea5c52b1cb453a0057c
Rivera84 <30479941+Rivera84@users.noreply.github.com>
2019-10-23 19:48:55 -0600

    commit casi final

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,17 @@
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
+    val elegirPiso = Pisos()
+    println("El elevador se encuentra detenido en el lobby")
+
+    //inicio del Juego
+    println("Ingrese el piso al que desea ir:")
+    val piso = readLine()!!.toInt()
+
+            if (piso <= 12) {
+                elegirPiso.estadoElevador(piso)
+            } else {
+                println("Este piso no existe")
+            }
+
 
 
 }
